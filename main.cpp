ca14957d04cc851e4a6bd9a6bc818fb0ebf42407
Bryan <bbarrios@student.msjc.edu>
2020-05-22 00:59:24 -0700

    Question 1 for loop

--- main.cpp
+++ main.cpp
@@ -3,16 +3,19 @@
 using namespace std;
 int main() {
 
+
    int Numbers;
 
 
     cout << "How many numbers would you like to enter?" << endl;
+    //user enters number
     cin>>Numbers;
 
     cout<<"enter a number";
-
+//check to see if statement is true to enter whileloop
  while(Numbers!=0)
  {
+     //declaration of vector initialized size to user input int
      vector<int>nums(Numbers);
 
      for(int i=0; i<Numbers; i++)
