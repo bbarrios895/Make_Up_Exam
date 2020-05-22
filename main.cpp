c4b3dad4ba3c0855b195938b4abb2e1f7a2cdbc1
Bryan <bbarrios@student.msjc.edu>
2020-05-22 03:33:57 -0700

    Question 1

--- main.cpp
+++ main.cpp
@@ -1,29 +1,53 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-int main() {
+void fill_array(int Numbers);
 
 
-   int Numbers;
 
 
-    cout << "How many numbers would you like to enter?" << endl;
-    //user enters number
-    cin>>Numbers;
+void  fill_array(int array[],int Size) {
 
-    cout<<"enter a number";
-//check to see if statement is true to enter whileloop
- while(Numbers!=0)
- {
-     //declaration of vector initialized size to user input int
-     vector<int>nums(Numbers);
+    for (int i = 0; i < Size; i++)
+    {
 
-     for(int i=0; i<Numbers; i++)
-  {
-
-  }
+cout<<array[i]<<"\t";
+    }
+cout<<endl;
 }
 
+int main() {
+
+ int Numbers;
+
+
+    cout << "How many numbers do you want to enter?" << endl;
+    //user enters number
+    cin>>Numbers;
+    int nums[Numbers];
+    //fill_array( Numbers);
+    //cout<<"enter a number";
+
+//check to see if statement is true to enter whileloop
+
+
+    int count=0;
+    for(int i=0; i<Numbers; i++)
+    {
+
+       if(cin>>nums[i])
+       {
+           count++;
+       }
+       else
+           {
+
+           break;
+       }
+
+
+    }
+ fill_array( nums,count);
 
     return 0;
 }
