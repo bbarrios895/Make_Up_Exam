72ca4ee799fdc5d45503628732acfaeda26ec8e9
bbarrios895 <61306221+bbarrios895@users.noreply.github.com>
2020-05-22 00:50:29 -0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main() {
+
+   int Numbers;
+
+
+    cout << "How many numbers would you like to enter?" << endl;
+    cin>>Numbers;
+
+    cout<<"enter a number";
+
+ while(Numbers!=0)
+ {
+     vector<int>nums(Numbers);
+
+     for(int i=0; i<Numbers; i++)
+  {
+
+  }
+}
+
+
+    return 0;
+}
