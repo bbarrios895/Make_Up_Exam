a1026531d62f21f3674d9b6995d57577636dddac
Bryan <bbarrios@student.msjc.edu>
2020-05-22 16:42:32 -0700

    Make up Exam Question 1

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <vector>
+
 
 using namespace std;
 
@@ -11,27 +11,27 @@ int main() {
 
     //declare and initialize variables to zero to search
     int largest = 0;
-    int smallest=0;
+    int smallest = 0;
 
-    cout << "How many numbers do you want to enter? "<<endl;
+    cout << "How many numbers do you want to enter? " << endl;
     // take user input to initialize variable
     cin >> UserInput;
 //for loop to run amount of times of user input value
     for (i = 0; i < UserInput; i++) {
         //request number from user input to fill spaces
-        cout << "Enter a number: \t"<<endl;
+        cout << "Enter a number: \t" << endl;
         cin >> number;
 //statement to search for largest numbers
         if (number > largest)
             //initializing smallest number to largest variable
             largest = number;
         //statement to search for smallest number
-        if (!i || smallest>number)
+        if (!i || smallest > number)
             //initializing smallest number to smallest variable
             smallest = number;
     }
 
-    cout<<"The largest number was"<< largest<<endl;
-    cout<<"The smallest number was"<< smallest<<endl;
+    cout << "The largest number was" << largest << endl;
+    cout << "The smallest number was" << smallest << endl;
     return 0;
 }
